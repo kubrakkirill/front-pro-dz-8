a4434d82e692f61c3cc3d32da58d35a68580d0ef
kubrakk <105593907+kubrakkirill@users.noreply.github.com>
2023-12-12 21:26:27 +0100

    fixed

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-const userInput = prompt('Enter array elements (only numbers with "," between)!')
-if(userInput){
-    const arr = userInput.split(',')
+const userInput = prompt('Enter array elements (only numbers with "," between)!');
+
+if (userInput) {
+    const arr = userInput.split(',');
     let sum = 0;
-    for(let i=0; i <= arr.length - 1; i++){
-        if(arr[i] === ''){
-            arr.splice(arr[i],1)
-        } else if(isNaN(arr[i])){
-            alert(`"${arr[i]}" not a number, I wouldn't sum up this element`)
-        } else if(!Number.isInteger(+arr[i])){
-            alert(`"${arr[i]}" isn't integer, I wouldn't sum up this element`)
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === '') {
+            arr.splice(i, 1);
+            i--;
+        } else if (isNaN(arr[i])) {
+            alert(`"${arr[i]}" not a number, I wouldn't sum up this element`);
+        } else if (!Number.isInteger(parseFloat(arr[i]))) {
+            alert(`"${arr[i]}" isn't an integer, I wouldn't sum up this element`);
         } else {
-            sum += parseFloat(arr[i])
+            sum += parseFloat(arr[i]);
         }
     }
-    alert(sum)
+
+    alert(sum);
 }
